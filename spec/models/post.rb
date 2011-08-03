01e6a6df0fb40cd13c422841e4c77e813389fcb1
Peter Savichev (proton) <psavichev@gmail.com>
2011-08-04 02:12:28 +0400

    updated specs

---
+++ spec/models/post.rb
@@ -0,0 +1,8 @@
+class Post
+  include Mongoid::Document
+
+  field :title
+
+  auto_increment :key, :seed => 500
+  auto_increment :num
+end
--- spec/mongoid_auto_increment_spec.rb
+++ spec/mongoid_auto_increment_spec.rb
@@ -9,6 +9,9 @@ describe "mongoid_auto_increment" do
     @order1 = Order.create :name => "First Order"
     @order2 = Order.create :name => "Second Order"
     @order3 = Order.create :name => "Last Order"
+    @post1 = Post.create :name => "First Post"
+    @post2 = Post.create :name => "Second Post"
+    @post3 = Post.create :name => "Last Post"
   end
 
   it "should have id 1" do
@@ -33,5 +36,29 @@ describe "mongoid_auto_increment" do
 
   it "should have id 1003" do
     @order3.num.should eql 1003
+  end
+
+  it "should have id 1" do
+    @post1.key.should eql 501
+  end
+
+  it "should have id 2" do
+    @post2.key.should eql 502
+  end
+
+  it "should have id 3" do
+    @post3.key.should eql 503
+  end
+
+  it "should have id 1" do
+    @post1.num.should eql 1
+  end
+
+  it "should have id 2" do
+    @post2.num.should eql 2
+  end
+
+  it "should have id 3" do
+    @post3.num.should eql 3
   end
 end
